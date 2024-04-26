b00c256f96b1eacaf5d351ec2fa746535e5fcef3
GiovanniPalmieri <48354624+GiovanniPalmieri@users.noreply.github.com>
2024-04-26 11:27:51 +0200

    started task controller

---
+++ TestProject/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TestProject.Models;
+using TestProject.Services;
+
+namespace TestProject.Controllers {
+    [ApiController]
+    [Route("api/project/{projectId}/task")]
+    public class TaskController : Controller {
+        private readonly IGiraRepository _giraRepository;
+        private readonly IMapper _mapper;
+
+        public TaskController(IGiraRepository giraRepository, IMapper mapper) {
+            _giraRepository = giraRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskDto>>> GetTasks(int projectId) {
+            if(! await _giraRepository.ProjectExistsAsync(projectId)) {
+                return NotFound();
+            }
+            var tasks = await _giraRepository.GetTasksForProjectAsync(projectId);
+            return Ok(_mapper.Map<IEnumerable<TaskDto>>(tasks));
+        }
+    }
+}
