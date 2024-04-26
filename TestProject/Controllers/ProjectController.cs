14113d28eba36d927570aadb07ae2f27a2280b18
GiovanniPalmieri <48354624+GiovanniPalmieri@users.noreply.github.com>
2024-04-26 11:26:49 +0200

    added all methods for projects

--- TestProject/Controllers/ProjectController.cs
+++ TestProject/Controllers/ProjectController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using TestProject.Entities;
 using TestProject.Models;
 using TestProject.Services;
 
@@ -21,11 +22,42 @@ namespace TestProject.Controllers {
             var projects = await _giraRepository.GetProjectsAsync();
             return Ok(_mapper.Map<IEnumerable<ProjectDto>>(projects));
         }
-        [HttpGet("{id}")]
+
+        [HttpGet("{id}", Name ="GetProject")]
         public async Task<ActionResult<IEnumerable<ProjectDto>>> GetAssignedProjects(int id) {
             var projects = await _giraRepository.GetAssignedProjectsAsync(id);
             return Ok(_mapper.Map<IEnumerable<ProjectDto>>(projects));
         }
 
+        [HttpDelete("{projectId}")]
+        public async Task<ActionResult> DeleteProject(int projectId) {
+            if(!await _giraRepository.ProjectExistsAsync(projectId)) {
+                return NotFound();
+            }
+            
+            await _giraRepository.DeleteProjectAsync(projectId);
+
+            await _giraRepository.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProjectDto>> CreateProject(ProjectForCreationDto project) {
+            var manager = await _giraRepository.GetManagerAsync(project.managerId);
+            if(manager == null) {
+                return NotFound(); 
+            }
+
+            var finalProject = _mapper.Map<Project>(project);
+            finalProject.Manager = manager;
+
+            await _giraRepository.AddProjectAsync(finalProject);
+
+            await _giraRepository.SaveChangesAsync();
+
+            var projectToReturn = _mapper.Map<ProjectToReturnDto>(finalProject);
+
+            return CreatedAtRoute("GetProject", new { id = projectToReturn.Id}, projectToReturn);
+        }
     }
 }
